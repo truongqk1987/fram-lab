2070a637926db30785e07e7b56cab8c6e0a40b4c
TruongQK1987 <truongqk1987@gmail.com>
2017-02-17 08:01:49 +0700

    Migrate Orders Table

---
+++ database/migrations/2017_02_16_234210_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->date('payment_date');
+            $table->integer('person_id');
+            $table->string('playing_currency');
+            $table->string('playing_original_amount');
+            $table->index('person_id');
+            $table->index('payment_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
