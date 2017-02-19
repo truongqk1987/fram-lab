3f85cb85cf6eabb16f2a863e80e2dd17f7dddcef
TruongQK1987 <truongqk1987@gmail.com>
2017-02-19 20:14:35 +0700

    Edit HTML

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel</title>
+    <title>Dashboard</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     <link href="{{asset('css/react-select.min.css')}}" rel="stylesheet" type="text/css">
