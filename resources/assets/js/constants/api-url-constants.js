31be68c7bbaeecb6fa6ea3eaee0b6c083153a977
TruongQK1987 <truongqk1987@gmail.com>
2017-02-19 14:04:59 +0700

    Edit URL constants

--- resources/assets/js/constants/api-url-constants.js
+++ resources/assets/js/constants/api-url-constants.js
@@ -1,7 +1,7 @@
 "use strict";
 var APIURLConstants = {
-    TOTAL_ORDERS_BY_CURRENCY: '/orders/totalOrdersByCurrency',
-    TOTAL_ORDERS_BY_PERSON: '/orders/totalOrdersByPerson',
-    TOTAL_ORDERS_MONTHLY: '/orders/totalOrdersMonthly'
+    TOTAL_ORDERS_BY_CURRENCY: 'orders/totalOrdersByCurrency',
+    TOTAL_ORDERS_BY_PERSON: 'orders/totalOrdersByPerson',
+    TOTAL_ORDERS_MONTHLY: 'orders/totalOrdersMonthly'
 }
 module.exports = APIURLConstants;
