e428edc54311eafecff4c457caf11409425a13ac
khaitruong <quankhai.truong@planet1world.com>
2017-02-20 13:32:25 +0700

    Use secure asset

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,16 +4,16 @@
     <title>Dashboard</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/react-select.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/loader.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/react-table.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/dashboard.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('css/react-select.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('css/loader.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('css/react-table.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('css/dashboard.min.css')}}" rel="stylesheet" type="text/css">
 
 </head>
 <body>
     <div id="dashboard-container">
 
     </div>
-    <script src="{{asset('js/dashboard.bundle.js')}}"></script>
+    <script src="{{secure_asset('js/dashboard.bundle.js')}}"></script>
 </body>
 </html>
